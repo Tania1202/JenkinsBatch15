dc1de5360b784e83101249b8331bfca2c7bdc97c
Tania1202 <lta120292@gmail.com>
2023-06-02 13:33:41 -0400

    Minor change

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         System.out.println("Hello world!");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Hello world!");
-            Thread.sleep(2000);
+            Thread.sleep(1000);
 
         }
     }
