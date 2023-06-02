c400ea19ac8c1676247fd2fec6914a3c821d0210
Tania1202 <lta120292@gmail.com>
2023-06-02 09:38:51 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Hello world!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world!");
+            Thread.sleep(2000);
+
+        }
+    }
+
+}
